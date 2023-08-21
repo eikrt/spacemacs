c64801d70c4f9e6d57d1648acc3a00e9830fdce2
Eino Korte <e.i.korte@gmail.com>
2023-08-21 13:43:39 +0300

    added comfy kb configs

--- init.el
+++ init.el
@@ -74,3 +74,21 @@
       (unless (server-running-p)
         (message "Starting a server...")
         (server-start)))))
+(global-set-key (kbd "M") nil)
+		      (define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+		(evil-define-command cofi/maybe-exit ()
+		  :repeat change
+		  (interactive)
+		  (let ((modified (buffer-modified-p)))
+		    (insert "j")
+		    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+			       nil 0.5)))
+		      (cond
+		       ((null evt) (message ""))
+		       ((and (integerp evt) (char-equal evt ?j))
+		    (delete-char -1)
+		    (set-buffer-modified-p modified)
+		    (push 'escape unread-command-events))
+		       (t (setq unread-command-events (append unread-command-events
+					  (list evt))))))))
+
